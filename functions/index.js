21eb2f2640b48729efcbcb040440d1e806cfb347
Wellington N <wellingto_07@hotmail.com>
2020-09-29 14:42:14 -0300

    token user auth, validation

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
+const { loginUser } = require("./APIs/users");
+
 const {
   listAllTodo,
   createTodo,
@@ -8,9 +10,13 @@ const {
   editTodo,
 } = require("./APIs/todos");
 
+//todo
 app.get("/todos", listAllTodo);
 app.post("/todo", createTodo);
 app.delete("/todo/:todoId", deleteTodo);
 app.put("/todo/:todoId", editTodo);
+
+//user
+app.post("/login", loginUser);
 
 exports.api = functions.https.onRequest(app);
