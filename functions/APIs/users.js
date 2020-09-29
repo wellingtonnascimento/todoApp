8d571c4b51fd5653fca1867592999f6c6f99ed8b
Wellington N <wellingto_07@hotmail.com>
2020-09-29 15:36:34 -0300

    update derails user

--- functions/APIs/users.js
+++ functions/APIs/users.js
@@ -89,6 +89,21 @@ exports.signUpUser = (request, response) => {
     });
 };
 
+exports.updateUserDetails = (request, response) => {
+  let document = db.collection("users").doc(`${request.user.email}`);
+  document
+    .update(request.body)
+    .then(() => {
+      response.json({ message: "Updated successfully" });
+    })
+    .catch((error) => {
+      console.error(error);
+      return response.status(500).json({
+        message: "Cannot Update the value",
+      });
+    });
+};
+
 deleteImage = (imageName) => {
   const bucket = admin.storage().bucket();
   const path = `${imageName}`;
--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ const {
   signUpUser,
   uploadProfilePhoto,
   getUserDetail,
+  updateUserDetails,
 } = require("./APIs/users");
 
 const {
@@ -17,15 +18,16 @@ const {
 } = require("./APIs/todos");
 
 //todo
-app.get("/todos", listAllTodo);
-app.post("/todo", createTodo);
-app.delete("/todo/:todoId", deleteTodo);
-app.put("/todo/:todoId", editTodo);
+app.get("/todos", auth, listAllTodo);
+app.post("/todo", auth, createTodo);
+app.delete("/todo/:todoId", auth, deleteTodo);
+app.put("/todo/:todoId", auth, editTodo);
 
 //user
 app.post("/login", loginUser);
 app.post("/signup", signUpUser);
 app.post("/user/image", auth, uploadProfilePhoto);
 app.get("/user", auth, getUserDetail);
+app.post("/user", auth, updateUserDetails);
 
 exports.api = functions.https.onRequest(app);
