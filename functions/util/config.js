8c6052976cfb8d608f4a85ff8dd0ab97119b3867
Wellington N <wellingto_07@hotmail.com>
2020-09-29 14:38:12 -0300

    config firebase

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCGLWD5jxMAs9VXMMSIxPcJjM54J7ibfQU",
+  authDomain: "rstcom-8c4e1.firebaseapp.com",
+  databaseURL: "https://rstcom-8c4e1.firebaseio.com",
+  projectId: "rstcom-8c4e1",
+  storageBucket: "rstcom-8c4e1.appspot.com",
+  messagingSenderId: "336109809917",
+  appId: "1:336109809917:web:92293affdf974a8a930e94",
+  measurementId: "G-8KW6ZM1MRS",
+};
