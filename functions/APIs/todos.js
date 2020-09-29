8e7067d6268cd7658a77bbcd2a937c92d956b229
Wellington N <wellingto_07@hotmail.com>
2020-09-29 14:31:15 -0300

    update todo

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -61,3 +61,21 @@ exports.deleteTodo = (request, response) => {
       return response.status(500).json({ error: err.code });
     });
 };
+
+exports.editTodo = (request, response) => {
+  if (request.body.todoId || request.body.createdAt) {
+    response.status(403).json({ message: "Not allowed to edit" });
+  }
+  let document = db.collection("todos").doc(`${request.params.todoId}`);
+  document
+    .update(request.body)
+    .then(() => {
+      response.json({ message: "Updated successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({
+        error: err.code,
+      });
+    });
+};
--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { listAllTodo, createTodo, deleteTodo } = require("./APIs/todos");
+const {
+  listAllTodo,
+  createTodo,
+  deleteTodo,
+  editTodo,
+} = require("./APIs/todos");
 
 app.get("/todos", listAllTodo);
 app.post("/todo", createTodo);
 app.delete("/todo/:todoId", deleteTodo);
+app.put("/todo/:todoId", editTodo);
 
 exports.api = functions.https.onRequest(app);
