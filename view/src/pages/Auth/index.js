474300b75d1962141afce7822e4849a417a9d5b2
Wellington N <wellingto_07@hotmail.com>
2020-09-30 22:00:16 -0300

    container auth

---
+++ view/src/pages/Auth/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+
+import LoginForm from "../../components/LoginForm";
+import CadastroForm from "../../components/CadastroForm";
+import { Container, Row, Col } from "react-bootstrap";
+import Logo from "../../assets/logo-rstcom-ok-.png";
+import "./styled.scss";
+
+export default function Auth() {
+  return (
+    <Container fluid className="d-flex flex-grow-1 flex-row-reverse">
+      <Row className="flex-fill flex-row">
+        <Col className=" d-none d-md-flex align-items-center  ">
+          <img className="logo" src={Logo} alt="" width="200" height="200" />
+        </Col>
+
+        <Col
+          xs={12}
+          lg={4}
+          md={6}
+          className="bg-light justify-content-center align-items-center col-lg-4 col-md-6 col-12 d-flex"
+        >
+          <Route path="/" exact component={LoginForm} />
+          <Route path="/cadastro" component={CadastroForm} />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
