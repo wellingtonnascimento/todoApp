cfabd45bf1819d0bfada852875f913dca73028c3
Wellington N <wellingto_07@hotmail.com>
2020-10-01 01:23:22 -0300

    components CadastroForm

--- view/src/components/CadastroForm/index.js
+++ view/src/components/CadastroForm/index.js
@@ -1,20 +1,79 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { Form, Button } from "react-bootstrap";
 
+import { AuthContext } from "../../contexts/auth-context";
+
 export default function CadastroForm() {
+  const { signup } = useContext(AuthContext);
+
+  const [state, setState] = useState({
+    firstName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  function handleFirstNameChange(event) {
+    event.persist();
+    setState((prevState) => ({ ...prevState, firstName: event.target.value }));
+  }
+
+  function handleEmailChange(event) {
+    event.persist();
+    setState((prevState) => ({ ...prevState, email: event.target.value }));
+  }
+  function handlePasswordChange(event) {
+    event.persist();
+    setState((prevState) => ({ ...prevState, password: event.target.value }));
+  }
+
+  function handleConfirmPasswordChange(event) {
+    event.persist();
+    setState((prevState) => ({
+      ...prevState,
+      confirmPassword: event.target.value,
+    }));
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await signup(state);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <Form className="col-10  ">
+    <Form className="col-10 " onSubmit={onSubmit}>
       <h5 className="text-center">Faça seu Login</h5>
       <br />
-      <Form.Control type="name" placeholder="Nome" />
+      <Form.Control
+        value={state.firstName}
+        onChange={handleFirstNameChange}
+        placeholder="Nome"
+      />
       <br />
-      <Form.Control type="email" placeholder="E-mail" />
+      <Form.Control
+        onChange={handleEmailChange}
+        type="email"
+        placeholder="E-mail"
+      />
       <br />
-      <Form.Control type="password" placeholder="Senha" />
+      <Form.Control
+        onChange={handlePasswordChange}
+        type="password"
+        placeholder="Senha"
+      />
       <br />
-      <Form.Control type="password" placeholder="Confirmar Senha" />
+      <Form.Control
+        onChange={handleConfirmPasswordChange}
+        type="password"
+        placeholder="Confirmar Senha"
+      />
       <br />
       <Button className="col-12" type="submit">
         Cadastrarr
