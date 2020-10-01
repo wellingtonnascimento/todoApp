da8f65a20b81743f97282b6ebdf63788ec7453c8
Wellington N <wellingto_07@hotmail.com>
2020-10-01 11:31:02 -0300

    list todo

--- view/src/components/TodoForm/index.js
+++ view/src/components/TodoForm/index.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { Form, Button } from "react-bootstrap";
+import { Form, Row, Col, Button } from "react-bootstrap";
+
+import api from "../../services/api";
 
 export default function LoginForm() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function loadTodo() {
+      const { data } = await api.get("todos");
+      setTodos(data);
+    }
+    loadTodo();
+  }, [setTodos]);
+
   return (
-    <Form className="col-10 border-radius">
-      <h5 className="text-center">Faça seu Login</h5>
-      <Form.Control type="email" placeholder="Enter email" />
-      <br />
-      <Form.Control type="password" placeholder="Password" />
-      <br />
-      <Button className="col-12" type="submit">
-        Entrar
-      </Button>
-    </Form>
+    <Row className="flex-fill flex-row mr-3">
+      <Col className="px-3 pb-5 ">
+        <div className="d-flex">
+          <span className="text-dark">/Todo list</span>
+        </div>
+        <Col className="px-4 pb-5">
+          <Row className="text-dark">
+            <span>Lista de Tarefas</span>
+          </Row>
+        </Col>
+        <Col className="">
+          {todos.map((todo) => (
+            <Row keys={todo.todoId}>
+              <a className="text-dark  ">{todo.title}</a>
+            </Row>
+          ))}
+        </Col>
+        <Col>
+          <Row>
+            <Form.Control
+              className="col-10 "
+              type="text"
+              placeholder="Escreva aqui sua tarefa"
+            />
+            <Button className="col-2" type="submit">
+              Entrar
+            </Button>
+          </Row>
+        </Col>
+      </Col>
+    </Row>
   );
 }
