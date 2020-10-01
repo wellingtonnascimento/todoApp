5323d233df733b988ebb541e7eafba9b19b15e57
Wellington N <wellingto_07@hotmail.com>
2020-10-01 01:25:59 -0300

    auth user

--- view/src/App.js
+++ view/src/App.js
@@ -1,17 +1,39 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom";
+import api from "./services/api";
+
 import "./App.scss";
 
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 
+import AuthContextProvider from "./contexts/auth-context";
+
 function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response && error.response.statusCode === 403) {
+          localStorage.clear();
+          history.push("/");
+        }
+        return Promise.reject(error);
+      }
+    );
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/(cadastro)?" exact component={Auth} />
-        <Route path="/home" component={Home} />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/(cadastro)?" exact component={Auth} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
