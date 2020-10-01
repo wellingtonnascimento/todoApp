a5fe6e28d512e59b8acccf64ee1201550f9dad84
Wellington N <wellingto_07@hotmail.com>
2020-10-01 01:24:11 -0300

    componets LoginFrom

--- view/src/components/LoginForm/index.js
+++ view/src/components/LoginForm/index.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { Form, Button } from "react-bootstrap";
 
+import { AuthContext } from "../../contexts/auth-context";
+
 export default function LoginForm() {
+  const { login } = useContext(AuthContext);
+
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleEmailChange(event) {
+    event.persist();
+    setState((prevState) => ({ ...prevState, email: event.target.value }));
+  }
+  function handlePasswordChange(event) {
+    event.persist();
+    setState((prevState) => ({ ...prevState, password: event.target.value }));
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await login(state);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <Form className="col-10 border-radius">
+    <Form className="col-10 border-radius" onSubmit={onSubmit}>
       <h5 className="text-center">Faça seu Login</h5>
-      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Control
+        onChange={handleEmailChange}
+        type="email"
+        placeholder="Enter email"
+      />
       <br />
-      <Form.Control type="password" placeholder="Password" />
+      <Form.Control
+        onChange={handlePasswordChange}
+        type="password"
+        placeholder="Password"
+      />
       <br />
       <Button className="col-12" type="submit">
         Entrar
