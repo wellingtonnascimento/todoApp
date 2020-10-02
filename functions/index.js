b1b938a171ef600ab661f572e6ca9438a9154f7f
Wellington N <wellingto_07@hotmail.com>
2020-10-02 01:29:12 -0300

    correction rota

--- functions/index.js
+++ functions/index.js
@@ -32,6 +32,6 @@ app.post("/login", loginUser);
 app.post("/signup", signUpUser);
 app.post("/user/image", auth, uploadProfilePhoto);
 app.get("/user", auth, getUserDetail);
-app.post("/user", auth, updateUserDetails);
+app.put("/user", auth, updateUserDetails);
 
 exports.api = functions.https.onRequest(app);
